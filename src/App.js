a3e7b490db0cc0a2df1c4516b6166e1b30726252
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-03 12:24:42 -0600

    Finish input templates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dropdown } from 'primereact/dropdown';
+
+const options = ["Ciudad 1","Ciudad 2","Ciudad 3"];
+
 function App() {
   return (
     <div>
-      App
+      <div className='p-inputgroup'>
+      <span className="p-inputgroup-addon">
+        <i className="pi pi-user"></i>
+        </span>
+        <InputText placeholder='Nombre'/>
+      </div>
+      <div className='p-inputgroup'>
+      <span className="p-inputgroup-addon">
+        <i className="pi pi-calendar"></i>
+        </span>
+        <InputNumber/>
+      </div>
+      <div className='p-inputgroup'>
+      <span className="p-inputgroup-addon">
+        <i className="pi pi-calendar"></i>
+        </span>
+        <Dropdown placeholder='Selecciona un país' options={options}/>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import "primereact/resources/themes/lara-light-indigo/theme.css"; 
+import "primereact/resources/primereact.min.css";                  
+import "primeicons/primeicons.css";   
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
