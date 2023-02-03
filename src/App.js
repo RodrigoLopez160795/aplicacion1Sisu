c9a4dc66167b2e4842a7f4500dadb804e350dd50
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-03 13:44:40 -0600

    Finish input with functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import CreateAccount from "./components/CreateAccount";
+import React, { useState } from "react";
+import { Dropdown } from 'primereact/dropdown';
 
 
 function App() {
@@ -7,6 +9,23 @@ function App() {
       <CreateAccount/>
     </div>
   );
+  // const [value, setValue] = useState(null);
+  //   const cities = [
+  //       { name: 'New York', code: 'NY' },
+  //       { name: 'Rome', code: 'RM' },
+  //       { name: 'London', code: 'LDN' },
+  //       { name: 'Istanbul', code: 'IST' },
+  //       { name: 'Paris', code: 'PRS' }
+  //   ];
+
+  //   return (
+  //       <div className="card flex justify-content-center">
+  //           <span className="p-float-label">
+  //               <Dropdown inputId="dropdown" value={value} options={cities} onChange={(e) => setValue(e.value)} optionLabel="name" />
+  //               <label htmlFor="dropdown">Dropdown</label>
+  //           </span>
+  //       </div>
+  //   )
 }
 
 export default App;
--- src/components/CreateAccount/index.js
+++ src/components/CreateAccount/index.js
@@ -1,14 +1,13 @@
 import { InputText } from "primereact/inputtext";
 import { InputNumber } from "primereact/inputnumber";
 import { Dropdown } from "primereact/dropdown";
-
-const options = {
-  countries: ["País 1", "País 2", "País 3"],
-  states: ["Estado 1", "Estado 2", "Estado 3"],
-  cities: ["Ciudad 1", "Ciudad 2", "Ciudad 3"],
-};
+import { useState } from "react";
+import { cities, countries, states } from "./options";
 
 function CreateAccount() {
+    const [country,setCountry]=useState(null);
+    const [state,setState] = useState(null);
+    const [city,setCity] = useState(null);
   return (
     <form>
       <div className="p-inputgroup">
@@ -34,7 +33,12 @@ function CreateAccount() {
           <i className="pi pi-map"></i>
         </span>
         <span className="p-float-label">
-          <Dropdown options={options.countries} inputId="countries" />
+          <Dropdown
+            options={countries}
+            inputId="countries"
+            onChange={({ value }) => setCountry(value)}
+            value={country}
+          />
           <label htmlFor="countries">Selecciona un país</label>
         </span>
       </div>
@@ -44,9 +48,11 @@ function CreateAccount() {
         </span>
         <span className="p-float-label">
           <Dropdown
-            options={options.states}
+            options={states}
             inputId="states"
-            className="p-disabled"
+            className={country ? "" : "p-disabled"}
+            onChange={({ value }) => setState(value)}
+            value={state}
           />
           <label htmlFor="states">Selecciona un estado</label>
         </span>
@@ -57,9 +63,11 @@ function CreateAccount() {
         </span>
         <span className="p-float-label">
           <Dropdown
-            options={options.cities}
+            options={cities}
             inputId="cities"
-            className="p-disabled"
+            className={state ? "" : "p-disabled"}
+            onChange={({ value }) => setCity(value)}
+            value={city}
           />
           <label htmlFor="cities">Selecciona una ciudad</label>
         </span>
@@ -69,3 +77,108 @@ function CreateAccount() {
 }
 
 export default CreateAccount;
+
+// DUDA EN ESTA FORMA DE HACER LAS COSAS
+
+// function CreateAccount() {
+//     const [values, setValues] = useState({
+//       countries: null,
+//       states: null,
+//       cities: null,
+//     });
+//     const options = [
+//       [
+//         { name: "País 1", code: "P1" },
+//         { name: "País 2", code: "P2" },
+//         { name: "País 3", code: "P3" },
+//       ],
+//       ["Estado 1", "Estado 2", "Estado 3"],
+//       ["Ciudad 1", "Ciudad 2", "Ciudad 3"],
+//     ];
+//     function handler(e, type) {
+//       switch (type) {
+//         case "countries":
+//           setValues({ ...values, countries: e.code });
+//           break;
+//         case "states":
+//           setValues({ ...values, states: e.code });
+//           break;
+//         case "cities":
+//           setValues({ ...values, cities: e.code });
+//           break;
+//       }
+//     }
+//     return (
+//       <form>
+//         <div className="p-inputgroup">
+//           <span className="p-inputgroup-addon">
+//             <i className="pi pi-user"></i>
+//           </span>
+//           <span className="p-float-label">
+//             <InputText id="name" />
+//             <label htmlFor="name">Nombre completo</label>
+//           </span>
+//         </div>
+//         <div className="p-inputgroup">
+//           <span className="p-inputgroup-addon">
+//             <i className="pi pi-calendar"></i>
+//           </span>
+//           <span className="p-float-label">
+//             <InputNumber />
+//             <label htmlFor="age">Edad</label>
+//           </span>
+//         </div>
+//         <div className="p-inputgroup">
+//           <span className="p-inputgroup-addon">
+//             <i className="pi pi-map"></i>
+//           </span>
+//           <span className="p-float-label">
+//             <Dropdown
+//               options={options[0]}
+//               inputId="countries"
+//               onChange={({ value }) => handler(value, "countries")}
+//               optionLabel="name"
+//               value={values.countries}
+//             />
+//             <label htmlFor="countries">Selecciona un país</label>
+//           </span>
+//         </div>
+//         <div className="p-inputgroup">
+//           <span className="p-inputgroup-addon">
+//             <i className="pi pi-map"></i>
+//           </span>
+//           <span className="p-float-label">
+//             <Dropdown
+//               options={options[1]}
+//               inputId="states"
+//               className="p-disabled"
+//               optionLabel="name"
+//             />
+//             <label htmlFor="states">Selecciona un estado</label>
+//           </span>
+//         </div>
+//         <div className="p-inputgroup">
+//           <span className="p-inputgroup-addon">
+//             <i className="pi pi-map"></i>
+//           </span>
+//           <span className="p-float-label">
+//             <Dropdown
+//               options={options[2]}
+//               inputId="cities"
+//               className="p-disabled"
+//               optionLabel="name"
+//             />
+//             <label htmlFor="cities">Selecciona una ciudad</label>
+//           </span>
+//         </div>
+//         <button
+//           onClick={(e) => {
+//             e.preventDefault();
+//             console.log(values);
+//           }}
+//         >
+//           Check values
+//         </button>
+//       </form>
+//     );
+//   }
