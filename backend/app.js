c3eccb30aefccfe8b84eaf3dff185a79494de9ea
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-08 17:02:00 -0600

    Finish countries and states

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const { countries, states } = require("./data");
 const app = express();
 const port = 8080;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+//Devuelve la lista de países
+app.get("/paises", (req, res) => {
+  res.status(200).json(countries);
+});
+
+// Devuelve los estados por país
+app.get("/estados/:countryId", (req, res) => {
+  const { countryId } = req.params;
+  const statesArray = states.filter((state) => state.countryId == countryId);
+  if (statesArray.length === 0)
+    res.status(404).json({ message: "No se encontro" });
+  else res.status(200).json(statesArray);
 });
 
 app.listen(port, () => {
