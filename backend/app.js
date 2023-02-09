729d72a88ecb3d012796a528cc72ba00db1a3a69
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-08 18:00:59 -0600

    Finish login

--- backend/app.js
+++ backend/app.js
@@ -57,6 +57,16 @@ app.post("/usuarios", (req, res) => {
   }
 });
 
+// Iniciar sesión
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+  if (name === undefined || password === undefined)
+    res.status(403).json({ message: "Faltan datos" });
+  if (users.some((user) => user.name === name && user.password === password))
+    res.status(200).json({ message: "Bienvenido" });
+  else res.status(401).json({ message: "Credenciales inválidas" });
+});
+
 app.listen(port, () => {
   console.log(`Listen on port ${port}`);
 });
