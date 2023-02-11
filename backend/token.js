d6e71843bce243e764733fdc051c2a9ad5d496dd
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-10 19:05:48 -0600

    Finish jwt

--- backend/token.js
+++ backend/token.js
@@ -5,4 +5,25 @@ function createToken(user) {
   return jwt.sign(user, privateKey);
 }
 
-module.exports = { createToken };
+// function verifyToken(token) {
+//   jwt.verify(token, privateKey, function (err,decoded) {
+//     console.log(err,decoded)
+//     if (err) {
+//       return {
+//         name: "JsonWebTokenError",
+//         message: "Token invalido",
+//       };
+//     } else return true;
+//   });
+// }
+
+function verifyToken(token) {
+  let valid;
+  jwt.verify(token, privateKey , (err, decoded) => {
+    if (decoded) valid = true;
+    else valid = false;
+  });
+  return valid;
+}
+
+module.exports = { createToken, verifyToken };
