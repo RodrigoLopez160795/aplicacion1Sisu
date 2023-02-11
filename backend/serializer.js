c00cabfa6171d38168ef88b6fc5dac2b2621bb2b
RodrigoLopez160795 <rodrigo.lopez.160795@hotmail.com>
2023-02-10 18:25:21 -0600

    Changes in serializer

--- backend/serializer.js
+++ backend/serializer.js
@@ -1,13 +1,25 @@
-function userSerializer(user, token) {
-  return {
-    id: user.id,
-    name: user.name,
-    age: user.age,
-    country: user.country,
-    state: user.state,
-    city: user.city,
-    token,
-  };
+const { createToken } = require("./token");
+
+function userSerializer(user, needToken = true) {
+  if (needToken)
+    return {
+      id: user.id,
+      name: user.name,
+      age: user.age,
+      country: user.country,
+      state: user.state,
+      city: user.city,
+      token: createToken(user),
+    };
+  else
+    return {
+      id: user.id,
+      name: user.name,
+      age: user.age,
+      country: user.country,
+      state: user.state,
+      city: user.city,
+    };
 }
 
 module.exports = userSerializer;
